964799c7fc43eccb4099c34d816d3deced6e3264
Clayton Wieberg <55328479+klayza@users.noreply.github.com>
2024-11-05 04:59:00 -0700

    init

---
+++ app/src/main/java/com/example/daisy/MainActivity.kt
@@ -0,0 +1,40 @@
+// MainActivity.kt
+package com.example.daisy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                ) {
+                    WelcomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WelcomeScreen() {
+    Text(
+            text = "You are now free!",
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center
+    )
+}
